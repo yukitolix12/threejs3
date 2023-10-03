7968425c1ac478edb708adaf3d912e19cc8d0b95
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-10-03 14:29:11 +0900

    functionの初期化関数(init)を作成

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 import * as THREE from "./build/three.module.js"
 
+<<<<<<< HEAD
 let camera, scene, renderer;
+
+function init () {
+    
+}
+=======
+let camera, scene, renderer;
+>>>>>>> 221127c4766988eec8183292b510f1a4d3cf85c5
