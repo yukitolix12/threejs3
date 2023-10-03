0e2d07b6a8013af133472875f74ef0beed5823a8
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-10-03 14:34:06 +0900

    camera作成

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-import * as THREE from "./build/three.module.js"
+import * as THREE from "./build/three.module.js";
 
 
 let camera, scene, renderer;
 
 function init () {
-    
+    //camera
+    camera = new THREE.PerspectiveCamera(
+        40,
+        window.innerWidth / window.innerHeight,
+        1,
+        15000
+    );
 }
