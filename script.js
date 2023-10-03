a464f6bb3d74014bc477d2ec07c6d0c1e74bec30
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-10-03 14:55:17 +0900

    position.yz座標も作成

--- script.js
+++ script.js
@@ -28,5 +28,7 @@ function init () {
         const mesh = new THREE.Mesh(geometry, material);
 
         mesh.position.x = 8000 * (2.0 * Math.random() - 1.0);
+        mesh.position.y = 8000 * (2.0 * Math.random() - 1.0);
+        mesh.position.z = 8000 * (2.0 * Math.random() - 1.0);
     }
 }
